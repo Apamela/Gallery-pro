cf645d1b5f88efdcfcd6a29561c6fcd6e82314a6
Apamela <pamelab.desire@gmail.com>
2019-10-10 11:14:15 +0200

    the new code

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
@@ -27,17 +27,7 @@ def convert_dates(dates):
     return day
 def gallery_of_day(request):
     date = dt.date.today()
-
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request,'all-gallery/today-gallery.html',{"date": date,})
 def past_days_gallery(request,past_date):
     try:    
         # Converts data from the string Url
@@ -45,12 +35,9 @@ def past_days_gallery(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-gallery/past-gallery.html', {"date": date})
