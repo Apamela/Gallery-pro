ed060bebcc73122eaa2b7a0c237b3dfd410ef3d2
Apamela <pamelab.desire@gmail.com>
2019-10-10 17:39:30 +0200

    add the admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Editor,Article,tags
 # Register your models here.
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(tags)
+admin.site.register(Location)
+admin.site.register(category)
