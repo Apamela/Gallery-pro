c15ee2c2c8cbeeeb514579704ce2a9a810d9e337
Apamela <pamelab.desire@gmail.com>
2019-10-10 10:26:03 +0200

    the new code

--- gallery/views.py
+++ gallery/views.py
@@ -14,7 +14,7 @@ def gallery_of_day(request):
         </html>
             '''
     return HttpResponse(html)
-    def convert_dates(dates):
+def convert_dates(dates):
 
     # Function that gets the weekday number for the date.
     day_number = dt.date.weekday(dates)
@@ -24,3 +24,16 @@ def gallery_of_day(request):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+def gallery_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
